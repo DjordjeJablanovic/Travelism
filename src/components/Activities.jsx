6a11c37e7b8b0769a2071257dc461807e2af6888
Djordje <dj.jablanovic@hotmail.com>
2022-11-11 16:00:24 +0100

    overlay removed

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -13,12 +13,11 @@ const Activities = () => {
         <h2 className='text-[40px] md:text-[64px] font-bold mb-5 font-primary'>Browse By Activities</h2>
         <button className='border border-white w-[150px] md:w-[100px] h-[50px] rounded-lg hover:scale-105 duration-300'>View All</button>
        </div>
-       <div className='flex flex-col lg:flex-row justify-center items-center w-full'>
+       <div className='flex flex-col lg:flex-row justify-center lg:px-20 items-center w-full'>
         {/* left */}
-        <div className='relative  md:w-auto lg:px-0 hover:scale-105 duration-300'>
+        <div className='relative  md:w-auto  hover:scale-105 duration-300 rounded-2xl'>
           {/* overlay */}
-          <div className='w-full h-full absolute bg-black/20 max-w-[95%] rounded-2xl z-10'></div>
-          <img src={chillout} alt="chillout" className='max-w-[95%]'/>
+          <img src={chillout} alt="chillout"/>
           <div className='absolute bottom-[20px] lg:bottom-[40px] left-[20px] lg:left-[40px] z-20'>
             <h3 className='text-[30px] lg:text-[42px] text-white font-bold'>Chillout</h3>
             <p className='text-[16px] lg:text-[20px] text-gray-600'>18 Locations</p>
@@ -27,19 +26,17 @@ const Activities = () => {
         {/* right */}
         <div>
           {/* surfing */}
-          <div className='relative mx-auto w-full lg:w-[90%] xl:w-full lg:px-0  my-3 lg:my-0 hover:scale-105  duration-300'>
-            <div className='w-full h-full absolute bg-black/20 rounded-2xl z-10'></div>
-            <img src={surfing} alt="surf" />
+          <div className='relative  flex justify-center mx-auto lg:px-7 my-3 lg:my-0 hover:scale-105  duration-300'>
+            <img src={surfing} alt="surf" className='w-full' />
             <div className='absolute bottom-[20px] lg:bottom-[40px] left-[20px] lg:left-[40px] z-20'>
               <h3 className='text-[30px] lg:text-[42px] text-white font-bold'>Surfing</h3>
               <p className='text-[16px] lg:text-[20px] text-gray-600'>12 Locations</p>
             </div>
           </div>
           {/* extreme and camping */}
-          <div className='flex flex-row justify-between w-full lg:w-[85%] xl:w-full mx-auto lg:mt-3'>
+          <div className='flex flex-row  justify-between w-full lg:px-7 xl:w-full mx-auto lg:mt-3'>
             {/* extreme */}
-            <div className='relative hover:scale-105 duration-300'>
-              <div className='w-full h-full absolute bg-black/20 rounded-2xl z-10'></div>
+            <div className='relative hover:scale-105 duration-300 '>
               <img src={extreme} alt="extreme" />
               <div className='absolute bottom-[10px] lg:bottom-[40px] left-[20px] z-20'>
                 <h3 className='text-[20px] lg:text-[42px] text-white font-bold'>Extreme</h3>
@@ -48,7 +45,6 @@ const Activities = () => {
             </div>
             {/* camping */}
             <div className='relative lg:ml-3 hover:scale-105 duration-300'>
-              <div className='w-full h-full absolute bg-black/20 rounded-2xl z-10'></div>
               <img src={camping} alt="camping" />
               <div className='absolute bottom-[10px] lg:bottom-[40px] left-[20px] z-20'>
                 <h3 className='text-[20px] lg:text-[42px] text-white font-bold'>Camping</h3>
