4e5d2822db8f627bd2804b25484ccf6130cd07c2
Djordje <dj.jablanovic@hotmail.com>
2022-11-11 17:37:24 +0100

    fixed subscription part

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -22,7 +22,7 @@ const Subscribe = () => {
             {/* right side */}
             
             <div className=' flex flex-col items-center lg:items-start justify-center px-1 md:px-10'>
-                <h2 className='text-[24px] text-center lg:pt-5 md:text-[74px] font-bold mb-2 font-primary'>Subscribe us to get offers</h2>
+                <h2 className='text-[24px] text-center lg:pt-5 md:text-[74px] font-bold mb-2 font-primary'>Subscribe to get offers</h2>
                 <p className='py-2 mb-5 text-[16px] md:text-[18px]  px-3 lg:w-[800px] leading-[30px] md:leading-[36px] text-gray-700'>Auctor fermentum, sit iaculis aliquam commodo non. Metus odio sed purus justo. Eget egestas non ornare tincidunt ut. Id eget erat libero dui tellus viverra. Massa risus et at turpis adipiscing faucibus. At faucibus lorem sodales blandit nunc, faucibus consectetur tempor diam dui. Nam neque, non leo mauris pellentesque odio. </p>
                 <div className='w-full md:w-[670px] md:h-[90px] px-2 flex items-center justify-between  rounded-xl shadow-lg'>
                   <input type="text" placeholder='Please enter your email address' className='w-[90%] md:w-[500px] h-10 md:h-[70px] focus:outline-none'/>
